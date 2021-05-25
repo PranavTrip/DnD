2d2cc4261922c77012ee8c36c7a846bf5af06647
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2021-05-25 11:13:13 +0530

    Drag and Drop

---
+++ DragAndDrop/dnd.js
@@ -0,0 +1,29 @@
+let whiteBoxes = document.getElementsByClassName("whiteBox");
+let imgBox = document.getElementById("imgBox");
+// console.log(whiteBox, imgBox);
+
+imgBox.addEventListener("dragstart", (e) => {
+  e.target.className += " hold";
+  setTimeout(() => {
+    e.target.className = "hide";
+  }, 0);
+});
+
+imgBox.addEventListener("dragend", (e) => {
+  e.target.className = "imgBox";
+});
+
+for (whiteBox of whiteBoxes) {
+  whiteBox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  whiteBox.addEventListener("dragenter", (e) => {
+    e.target.className("dashed");
+  });
+  whiteBox.addEventListener("dragleave", (e) => {
+    e.target.className("whiteBox");
+  });
+  whiteBox.addEventListener("drop", (e) => {
+    e.target.append(imgBox);
+  });
+}
